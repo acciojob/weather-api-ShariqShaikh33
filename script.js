5be1766d1408934c94179e65703eebdfef0e1581
Shariq Shaikh <shariqshaikh2705@gmail.com>
2025-04-15 18:28:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let btn = document.getElementById("btn");
+let key = "f7292c79a58b639b384ff864b070a450";
+function handleClick(){
+	fetch("https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=f7292c79a58b639b384ff864b070a450")
+		.then((e)=>{
+			console.log(e);
+		})
+}
+
+btn.addEventListener("click",handleClick);
+
