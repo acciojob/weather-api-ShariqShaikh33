70128219c823e29e3e95a872d75fb89bf8aab68c
Shariq Shaikh <shariqshaikh2705@gmail.com>
2025-04-15 18:59:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 let btn = document.getElementById("btn");
 let key = "f7292c79a58b639b384ff864b070a450";
 async function handleClick(){
-	let response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=f7292c79a58b639b384ff864b070a450")
+	let response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=e467712b257e418838be97cc881a71de")
 	let data = response.json();
 	return data;
 }
