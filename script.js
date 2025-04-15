a7c5677d3701c61b58d652dfa747bc794330e66a
Shariq Shaikh <shariqshaikh2705@gmail.com>
2025-04-15 18:39:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 //your JS code here. If required.
 let btn = document.getElementById("btn");
 let key = "f7292c79a58b639b384ff864b070a450";
-function handleClick(){
-	fetch("https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=f7292c79a58b639b384ff864b070a450")
-		.then((e)=>{
-			console.log(e);
-		})
+async function handleClick(){
+	let response = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=19.07&lon=72.87&appid=f7292c79a58b639b384ff864b070a450")
+	let data = response.json();
+	return data;
 }
 
-btn.addEventListener("click",handleClick);
+btn.addEventListener("click",()=>{
+	handleClick()
+		.then((e)=>{
+			console.log(e.weather[0].main);
+			// ler result = e.
+			// let div=document.getElementById("weatherData")
+			// div.innerHTML="Current weather in Mumbai: "+result
+		})
+
+});
 
